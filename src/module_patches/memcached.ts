7fc8499c0255178aec0936b145404af1c3fc7e8e
Nick Johnstone <ncwjohnstone@gmail.com>
2020-12-18 17:57:07 +1300

    Update memcached patch, add tests
    Also ensure that memcached commands are wrapped in async resources to
    help propagate causality.

--- src/module_patches/memcached.ts
+++ src/module_patches/memcached.ts
@@ -2,69 +2,69 @@ import { executionAsyncId, AsyncResource } from 'async_hooks';
 import { EventEmitter } from 'events';
 import path from 'path';
 
-import { collectSample } from '../v8-profiler';
-
 import * as BI from '../bigint';
-import { recordQueryWithExitPoint } from '../async_effect_helpers';
+import { recordQuery } from '../effects';
 import { patchModules } from '../module_patches';
 import { makeClassCallable, wrapType } from '../async';
 import { QueryInformation } from '../types';
 
-patchModules(['memcached'], (exports) => {
-  const memcachedInternalCalls = new EventEmitter();
+export function load() {
+  patchModules(['memcached'], (exports) => {
+    const memcachedInternalCalls = new EventEmitter();
 
-  const originalCommand = exports.prototype.command;
+    const originalCommand = exports.prototype.command;
 
-  exports.prototype.command = function command<T>(this: T, ...args: any): any {
-    collectSample();
-    const recordQuery = recordQueryWithExitPoint('memcached');
-    const asyncId = executionAsyncId();
-    const [makeQuery, server, ...rest] = args;
+    exports.prototype.command = function command<T>(this: T, ...args: any): any {
+      const asyncResource = new AsyncResource('MEMCACHED_COMMAND');
+      const asyncId = executionAsyncId();
+      const [makeQuery, server, ...rest] = args;
 
-    const query = makeQuery();
-    const queryCallback = query.callback;
+      const query = makeQuery();
+      const queryCallback = query.callback;
 
-    const outerThis = this;
-    let actualServer = '';
+      const outerThis = this;
+      let actualServer = '';
 
-    memcachedInternalCalls.once('connect', (server: string) => {
-      actualServer = server;
-    });
-
-    query.callback = function handler<T, This>(
-      this: This,
-      error: Error | null,
-      data: T | undefined,
-    ) {
-      const duration = BI.subtract(BI.now(), startTime);
-      const result = queryCallback.call(this, error, data);
-
-      recordQuery({
-        startTime,
-        duration,
-
-        provider: 'memcached',
-        host: actualServer || server || '',
-        database: 'default',
-        query: query.command,
-
-        triggerAsyncId: asyncId,
+      memcachedInternalCalls.once('connect', (server: string) => {
+        actualServer = server;
       });
 
-      return result;
+      query.callback = function handler<T, This>(
+        this: This,
+        error: Error | null,
+        data: T | undefined,
+      ) {
+        const duration = BI.subtract(BI.now(), startTime);
+        const result = asyncResource.runInAsyncScope(queryCallback, this, error, data);
+
+        queryEffects.emit('complete', {
+          startTime,
+          duration,
+
+          provider: 'memcached',
+          host: actualServer || server || '',
+          database: 'default',
+          query: query.command,
+
+          triggerAsyncId: asyncId,
+        });
+
+        return result;
+      };
+
+      const startTime = BI.now();
+      const queryEffects = recordQuery('memcached', startTime, executionAsyncId());
+      return originalCommand.apply(this, [() => query, server, ...rest]);
     };
 
-    const startTime = BI.now();
-    return originalCommand.apply(this, [() => query, server, ...rest]);
-  };
+    const originalConnect = exports.prototype.connect;
 
-  const originalConnect = exports.prototype.connect;
+    exports.prototype.connect = function connect<T>(this: T, ...args: any[]) {
+      memcachedInternalCalls.emit('connect', ...args);
 
-  exports.prototype.connect = function connect<T>(this: T, ...args: any[]) {
-    memcachedInternalCalls.emit('connect', ...args);
+      return originalConnect.apply(this, args);
+    };
 
-    return originalConnect.apply(this, args);
-  };
-
-  return exports;
-});
+    return exports;
+  });
+}
